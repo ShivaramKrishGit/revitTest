39826ec7298527c43a6612518d95058678c90756
ShivaramKrishGit <shivaraman.krish@gmail.com>
2025-01-28 13:00:32 -0500

    Add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MeshOptimizer from './components/MeshOptimizer'
+
+function App() {
+  return (
+    <div>
+      <MeshOptimizer />
+    </div>
+  )
+}
+
+export default App
